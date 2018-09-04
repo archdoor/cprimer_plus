28f61e4c32d87d9a9436f17d6ab039d14206795b
archdoor <archdoor@outlook.com>
2018-09-04 18:16:14 +0800

    add files

---
+++ 3_1_platinum.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float weigth;
+    float value;
+
+    printf("Are you worth your weight in platinum?\n");
+    printf("Let's check it out.\n");
+    printf("Please enter you weight in pounds: \n");
+
+    scanf("%f", &weigth);
+
+    value = 1700.0 * weigth * 14.5833;
+    printf("Your weith in platinum is worth $%.2f.\n", value);
+    printf("You are easily worth that! If platinum prices drop,\n");
+    printf("eat more to maintain your value.\n");
+
+    return 0;
+}
+
