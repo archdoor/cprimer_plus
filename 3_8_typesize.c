8f23dd9eebeb57a8d700d7b2f393c6d7ebed467f
archdoor <archdoor@outlook.com>
2018-09-06 14:30:50 +0800

    add files

---
+++ 3_8_typesize.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("int: %zd bytes.\n", sizeof(int));
+    printf("char: %zd bytes.\n", sizeof(char));
+    printf("long: %zd bytes.\n", sizeof(long));
+    printf("long long: %zd bytes.\n", sizeof(long long));
+    printf("double: %zd bytes.\n", sizeof(double));
+    printf("long double: %zd bytes.\n", sizeof(long double));
+
+    return 0;
+}
+
+
